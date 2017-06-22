b0807326243063002bc845b1801b1697433da8ff
malloryb <mallorylbarnes@gmail.com>
2017-06-22 16:50:58 -0700

    Working on function to rasterize all modis NDVI data

--- 08_MODIS_HDF.R
+++ 08_MODIS_HDF.R
@@ -11,17 +11,40 @@ library(rgdal)
 #using the MODIS package
 #For 1 grid file: 
 
-#This is taking awhile
+gdalinfo("C:/Users/rsstudent/odrive/UA_Google_Drive/Upscaling_Data/MOD13A1_Col6/MOD13A1.A2000049.h07v06.006.2015136104433.hdf")
+
+sds <- get_subdatasets("C:/Users/rsstudent/odrive/UA_Google_Drive/Upscaling_Data/MOD13A1_Col6/MOD13A1.A2000049.h07v06.006.2015136104433.hdf")
+
+sds[1]
+gdal_translate(sds[1], dst_dataset="NDVI_2000_49.tif")
+rast <- raster("NDVI_2000_49.tif")
+plot(rast)
+
+
+#For Many Files: 
+
+setwd("C:/Users/rsstudent/odrive/UA_Google_Drive/Upscaling_Data/MOD13A1_Col6/")
+
+
 
 gdalinfo("C:/Users/rsstudent/odrive/UA_Google_Drive/Upscaling_Data/MOD13A1_Col6/MOD13A1.A2000049.h07v06.006.2015136104433.hdf")
 
 sds <- get_subdatasets("C:/Users/rsstudent/odrive/UA_Google_Drive/Upscaling_Data/MOD13A1_Col6/MOD13A1.A2000049.h07v06.006.2015136104433.hdf")
-sds[1]
 
-my_hdf_layer <- raster(readGDAL(sds[1], as.is=TRUE))
+sds[1]
+gdal_translate(sds[1], dst_dataset="NDVI_2000_49.tif")
+rast <- raster("NDVI_2000_49.tif")
+plot(rast)
+
+#For multiple files
+files <- dir(pattern=".hdf$")
+
+convert_hdf_to_raster <- function(x){
+  
+}
 
 getSds(HdfName="C:/Users/rsstudent/odrive/UA_Google_Drive/Upscaling_Data/MOD13A1_Col6/MOD13A1.A2000049.h07v06.006.2015136104433.hdf")
 
 #Get layer x of an hdf file
 
-my_hdf_layer <- raster(readGDAL(sds$SDS4gdal[4], as.is. = TRUE))
+
