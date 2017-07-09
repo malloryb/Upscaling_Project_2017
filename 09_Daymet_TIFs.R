e1e94b5eb062f5577b569a3447efd73863cb2577
malloryb <mallorylbarnes@gmail.com>
2017-07-09 16:18:04 -0700

    Minor changes to file path - test if push/pull works

--- 09_Daymet_TIFs.R
+++ 09_Daymet_TIFs.R
@@ -2,6 +2,8 @@
 ## Load All Daymet .TIFs 
 
 ##Navigate to file with .TIFs
+setwd("C:/Users/Mallory/odrive/UA_Google_Drive/Daymet_V3_Monthly_Climatology/")
+
 ##Load all .TIFs into a list
 ##Create raster stack
 ##Subset spatially 
