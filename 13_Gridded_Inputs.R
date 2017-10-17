4b117bf66b2fbe9928908265c9c727335654d8f8
malloryb <mallorylbarnes@gmail.com>
2017-10-17 15:06:23 -0700

    Just trying to deal with Jan 2001 for now.

--- 13_Gridded_Inputs.R
+++ 13_Gridded_Inputs.R
@@ -1,5 +1,5 @@
 #Code to deal with rasters 
-#If you are looking for the file to makee TIFs from raw MODIS files see 9_25 tifs file (not in this Project)
+#If you are looking for the file to makee
 library(tiff)
 library(raster)
 library(rgdal)
@@ -7,6 +7,20 @@ library(lubridate)
 library(MODIS)
 library(ncdf4)
 library(MODIStsp)
+#For LST-----------------------------------------------
+#Set working directory
+setwd("C:/Users/Mallory/Documents/MRT/")
+#List of .TIF files to process
+filenames <- list.files("Params/", pattern= "*tif$")
+filenames_Jan <- list.files("Params/", pattern="*A200100.*tif$")
+
+
+rast_test <- raster("C:/Users/Mallory/Documents/MRT/Params/MOD11A2.A2001017.LST_Day_1km.tif")
+plot(rast_test)
+extent(rast_test)
+res(rast_test)
+
+
 #MeanIgnoringZeroes -> user-defined function to get mean without zeros
 
 meanIgnoringZeroes <- function(x) {
@@ -21,6 +35,11 @@ mean_na <- function(x) {
   mean(x,na.rm=T)
 }
 
+#Function to process MODIS LST 1km Daytime rasters
+#Function must (could do lapply): 
+#Read in TIFF file 
+#Extract Date from TIFF file
+#Rescale raster
 scale_raster <- function(x) {
   filename <- paste(x)
   print(filename)
@@ -35,71 +54,18 @@ scale_raster <- function(x) {
   return(rast)
 }
 
-s2 <- lapply(filenames, scale_raster)
-stack(s2)
+#For NDVI
 
-#Overlay rasters by month
-#TAKES FOREVER - 12:54pm to 
-Jan_mean <- overlay(s2[[1]], s2[[2]], s2[[3]], s2[[4]], fun=mean_na)
-Feb_mean <- overlay(s2[[5]], s2[[6]], s2[[7]], s2[[8]], fun=mean_na)
-Mar_mean <- overlay(s2[[9]], s2[[10]], s2[[11]], s2[[12]], fun=mean_na)
-Apr_mean <- overlay(s2[[13]], s2[[14]], s2[[15]], fun=mean_na)
-May_mean <- overlay(s2[[16]], s2[[17]], s2[[18]], s2[[19]], fun=mean_na)
-Jun_mean <- overlay(s2[[20]], s2[[21]], s2[[22]], fun=mean_na)
-Jul_mean <- overlay(s2[[23]], s2[[24]], s2[[25]], s2[[26]], fun=mean_na)
-Aug_mean <- overlay(s2[[27]], s2[[28]], s2[[29]], s2[[30]], fun=mean_na)
-Sep_mean <- overlay(s2[[31]], s2[[32]], s2[[33]], s2[[34]], fun=mean_na)
-Oct_mean <- overlay(s2[[35]], s2[[36]], s2[[37]], fun=mean_na)
-Nov_mean <- overlay(s2[[38]], s2[[39]], s2[[40]], s2[[41]], fun=mean_na)
-Dec_mean <- overlay(s2[[42]], s2[[43]], s2[[44]], s2[[45]], fun=mean_na)
-
-stk <- stack(Jan_mean,Feb_mean,Mar_mean,Apr_mean, May_mean, Jun_mean, Jul_mean, Aug_mean,Sep_mean, Oct_mean, Nov_mean, Dec_mean)
-names(stk) <- c('Jan','Feb','Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+#List of .TIF files to process
+filenames_NDVI <- list.files("NDVIParam/", pattern= "*tif$")
+filenames_Jan_NDVI <- list.files("NDVIParam/", pattern="*A200100.*tif$")
 
 
-writeRaster(stk, file="C:/Users/Mallory/Documents/MRT/Monthly_Daytime_LST_2001.nc") 
-
-s <- stack(Jan_mean, Apr_mean)
-
-#Plot
-plot(LST1)
-#Set all 0 values as NA
-LST1[LST1==0]<-NA
-#Rescale properly; https://gis.stackexchange.com/questions/72524/how-do-i-convert-the-lst-values-on-the-modis-lst-image-to-degree-celsius
-LST1 <- ((LST1 * 0.02) - 273.15)
-#Plot
-plot(LST1)
-
-#Read reasters from Guillermo
-#MODIS files each have 23 bands corresponding to each 16-day value
-#Daymet files have one band for each month from 2000-2016 -> 204 bands in file in total.
-
-Precip <- raster("F:/Upscaling_Project/Gridded_Inputs/Daymet/upscalingArea_DAYMET_prcp_2000_2016_AOI.tif")
-Tmax <- raster("F:/Upscaling_Project/Gridded_Inputs/Daymet/upscalingArea_DAYMET_tmax_2000_2016_AOI.tif")
-EVI <- raster("F:/Upscaling_Project/Gridded_Inputs/EVI/upscalingArea_2001_EVI.tif")
-str(EVI)
-str(Precip)
-setMinMax(Precip)
-Precip[Precip==-9999] <- NA 
-plot(Precip[[1]])
-Precip
-
-
-str(Tmax)
-setMinMax(Tmax)
-Tmax[Tmax==-9999] <- NA 
-plot(Tmax[[1]])
-Tmax
-
-plot(Precip)
-plot(Tmax)
-
-
-scale_raster <- function(x) {
+scale_raster_NDVI <- function(x) {
   filename <- paste(x)
   print(filename)
-  #rast <- raster(paste("NDVIParam", x, sep="/"))
-  ##rast_date <- extractDate(x, pos1=10, pos2=16, asDate=TRUE)
+  rast <- raster(paste("NDVIParam", x, sep="/"))
+  rast_date <- extractDate(x, pos1=10, pos2=16, asDate=TRUE)
   rast_date <- rast_date[[1]][1]
   date <- (as.character(rast_date))
   rast <- setMinMax(rast)
@@ -110,9 +76,69 @@ scale_raster <- function(x) {
 }
 
 
+s22 <-lapply(filenames_NDVI, scale_raster_NDVI)
+s32 <- lapply(filenames_Jan_NDVI, scale_raster_NDVI)
+
+plot(s32[[1]])
+
+#Overlay rasters by month
+#TAKES FOREVER
+Jan_mean <- overlay(s22[[1]], s22[[2]], fun=mean_na)
+Feb_mean <- overlay(s22[[3]], s22[[4]], fun=mean_na)
+Mar_mean <- overlay(s22[[5]], s22[[6]], fun=mean_na)
+Apr_mean <- overlay(s22[[7]], s22[[8]], fun=mean_na)
+May_mean <- overlay(s22[[9]], s22[[10]], fun=mean_na)
+Jun_mean <- overlay(s22[[11]], s22[[12]], fun=mean_na)
+Jul_mean <- overlay(s22[[13]], s22[[14]], fun=mean_na)
+Aug_mean <- overlay(s22[[15]], s22[[16]], fun=mean_na)
+Sep_mean <- overlay(s22[[17]], s22[[18]], fun=mean_na)
+Oct_mean <- overlay(s22[[19]],  fun=mean_na)
+Nov_mean <- overlay(s22[[20]], s22[[21]], fun=mean_na)
+Dec_mean <- overlay(s22[[22]], s22[[23]], fun=mean_na)
+
+stkNDVI <- stack(Jan_mean,Feb_mean,Mar_mean,Apr_mean, May_mean, Jun_mean, Jul_mean, Aug_mean,Sep_mean, Oct_mean, Nov_mean, Dec_mean)
+names(stkNDVI) <- c('Jan','Feb','Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+
+writeRaster(stkNDVI, file="C:/Users/Mallory/Documents/MRT/Monthly_NDVI_2001.nc", overwrite=TRUE) 
+memory.limit(size = 150000)
+memory.limit()
+gc()
+
+
+#Read rasters from Guillermo
+Precip <-raster("F:/Upscaling_Project/Gridded_Inputs/Daymet/upscalingArea_DAYMET_prcp_2000_2016_AOI.tif")
+Temp <- raster("F:/Upscaling_Project/Gridded_Inputs/Daymet/upscalingArea_DAYMET_tmax_2000_2016_AOI.tif")
+EVI <- raster("F:/Upscaling_Project/Gridded_Inputs/EVI/upscalingArea_2001_EVI.tif")
 
 plot(Precip[[1]])
-plot(Temp[[1]])
+setMinMax(Precip)
+Precip[Precip==-9999] <- NA 
+plot(Precip[[1]])
+Precip
+
+Jan_2001_precip <- raster("F:/Upscaling_Project/Gridded_Inputs/Daymet/upscalingArea_DAYMET_prcp_2000_2016_AOI.tif", 
+       band = 13)
+
+Jan_2001_precip[Jan_2001_precip==-9999] <- NA 
+plot(Jan_2001_precip)
+
+Jan_2001_temp <- raster("F:/Upscaling_Project/Gridded_Inputs/Daymet/upscalingArea_DAYMET_tmax_2000_2016_AOI.tif", 
+                        band = 13)
+
+Jan_2001_temp[Jan_2001_temp==-9999] <- NA 
+plot(Jan_2001_temp)
+
+Jan1 <- raster("F:/Upscaling_Project/Gridded_Inputs/EVI/upscalingArea_2001_EVI.tif", band=1)
+Jan2 <- raster("F:/Upscaling_Project/Gridded_Inputs/EVI/upscalingArea_2001_EVI.tif", band=2)
+
+Jan_2001_EVI <- overlay(Jan1, Jan2, fun=mean_na)
+
+Jan_2001_EVI[Jan_2001_EVI<0] <- NA 
+Jan_2001_EVI <- (Jan_2001_EVI * 0.0001)
+plot(Jan_2001_EVI)
+Jan_2001_EVI
+
 
 
 
